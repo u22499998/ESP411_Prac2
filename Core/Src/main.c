ad1fac516a26f90bf85e55faba375a2948156316
Edward Clements <u22499998@tuks.co.za>
2026-04-11 15:03:01 +0200

    Edited the filter and outpiut buffer enabled

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -40,7 +40,7 @@
 /* Private typedef -----------------------------------------------------------*/
 /* USER CODE BEGIN PTD */
 DisplayState_t currentState = STATE_MAIN_MENU;
-uint32_t dma_packed_buffer[513];
+uint32_t dma_packed_buffer[512];
 volatile uint8_t dma_data_ready = 0;
 
 /* USER CODE END PTD */
@@ -261,7 +261,7 @@ void SystemClock_Config(void)
   /** Configure the main internal regulator output voltage
   */
   __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -271,9 +271,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 4;
-  RCC_OscInitStruct.PLL.PLLN = 72;
+  RCC_OscInitStruct.PLL.PLLN = 144;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 3;
+  RCC_OscInitStruct.PLL.PLLQ = 6;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -285,10 +285,10 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV8;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV16;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
   {
     Error_Handler();
   }
@@ -512,7 +512,7 @@ static void MX_DAC_Init(void)
   /** DAC channel OUT2 config
   */
   sConfig.DAC_Trigger = DAC_TRIGGER_T7_TRGO;
-  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_DISABLE;
+  sConfig.DAC_OutputBuffer = DAC_OUTPUTBUFFER_ENABLE;
   if (HAL_DAC_ConfigChannel(&hdac, &sConfig, DAC_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
@@ -1085,32 +1085,32 @@ void StartDefaultTask(void const * argument)
 
 	                          // 2. Run the digital FIR filter using your new module
 	                          // This function takes the input array, processes it, and fills the output array
-	                          FIR_ProcessBlock(signal_samples, output_samples, 1024);
-
-	                          // 3. Convert floats back to 12-bit unsigned integers for the DAC
-	                          for (int i = 0; i < 1024; i++) {
-	                              // Add the 2048 DC offset back so the wave is centered properly
-	                              float dac_val = output_samples[i];
-
-	                              // Clamp values to prevent integer overflow/underflow screeching
-	                              if (dac_val > 4095.0f) dac_val = 4095.0f;
-	                              if (dac_val < 0.0f)    dac_val = 0.0f;
-
-	                              dac_buffer[i] = (uint16_t)dac_val;
-	                          }
-
-//	          				// Just copy the raw input straight to the DAC buffer
-//		          				for (int i = 0; i < 1024; i++) {
-//		          				    // Add the DC offset back
-//		          				    float dac_val = signal_samples[i];
+//	                          FIR_ProcessBlock(signal_samples, output_samples, 1024);
 //
-//		          				    // Clamp
-//		          				    if (dac_val > 4095.0f) dac_val = 4095.0f;
-//		          				    if (dac_val < 0.0f)    dac_val = 0.0f;
+//	                          // 3. Convert floats back to 12-bit unsigned integers for the DAC
+//	                          for (int i = 0; i < 1024; i++) {
+//	                              // Add the 2048 DC offset back so the wave is centered properly
+//	                              float dac_val = output_samples[i];
 //
-//		          				    dac_buffer[i] = (uint16_t)dac_val;
-//		          				    output_samples[i] = dac_val;
-//		          				}
+//	                              // Clamp values to prevent integer overflow/underflow screeching
+//	                              if (dac_val > 4095.0f) dac_val = 4095.0f;
+//	                              if (dac_val < 0.0f)    dac_val = 0.0f;
+//
+//	                              dac_buffer[i] = (uint16_t)dac_val;
+//	                          }
+
+	          				// Just copy the raw input straight to the DAC buffer
+		          				for (int i = 0; i < 1024; i++) {
+		          				    // Add the DC offset back
+		          				    float dac_val = signal_samples[i];
+
+		          				    // Clamp
+		          				    if (dac_val > 4095.0f) dac_val = 4095.0f;
+		          				    if (dac_val < 0.0f)    dac_val = 0.0f;
+
+		          				    dac_buffer[i] = (uint16_t)dac_val;
+		          				    output_samples[i] = dac_val;
+		          				}
 
 
 	          				// 3. Execute the Plotting State
